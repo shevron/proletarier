70c0885e77a8c8d44f223cbee1192ea57f1aa083
Shahar Evron <shahar.evron@gmail.com>
2014-05-10 16:00:09 +0300

    Updating classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -9,6 +9,7 @@ return array(
   'Proletarier\EventManagerAwareTrait'  => __DIR__ . '/src/Proletarier/EventManagerAwareTrait.php',
   'Proletarier\Handler\AbstractHandler' => __DIR__ . '/src/Proletarier/Handler/AbstractHandler.php',
   'Proletarier\Handler\EventLogger'     => __DIR__ . '/src/Proletarier/Handler/EventLogger.php',
+  'Proletarier\LoggingListener'         => __DIR__ . '/src/Proletarier/LoggingListener.php',
   'Proletarier\Worker\Worker'           => __DIR__ . '/src/Proletarier/Worker/Worker.php',
   'Proletarier\Worker\WorkerInterface'  => __DIR__ . '/src/Proletarier/Worker/WorkerInterface.php',
   'Proletarier\Worker\WorkerPool'       => __DIR__ . '/src/Proletarier/Worker/WorkerPool.php',
